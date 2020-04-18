73e37a8d8f4f1e8bf3127b9bceeec5775332ecb3
José Arteaga <nixon.jaag@gmail.com>
2020-04-18 12:31:49 -0400

    portafolio

---
+++ paginas/portafolio.js
@@ -0,0 +1,42 @@
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
+
+window.addEventListener('load', () => {
+	grid.refreshItems().layout();
+	document.getElementById('grid').classList.add('imagenes-cargadas');
+
+	// Agregamos los listener de los enlaces para filtrar por categoria.
+	const enlaces = document.querySelectorAll('#categorias a');
+	enlaces.forEach((elemento) => {
+		elemento.addEventListener('click', (evento) => {
+			evento.preventDefault();
+			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+			evento.target.classList.add('activo');
+
+			const catego = evento.target.innerHTML.toLowerCase();
+			catego === 'todos' ? grid.filter('[data-categoria]') : grid.filter((item)=>item.getElement().dataset.categoria.includes(catego));
+		});
+    });
+    // Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+		});
+	});
+
+	// Eventlistener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
+
+	// Eventlistener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+});
